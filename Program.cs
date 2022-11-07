8d430425ad8664e49c07d38bd40727da11d978dc
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-11-07 12:29:34 +0000

    Math constants work now too

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Sprache;
+
+namespace ALinqyCalculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            var line = "";
+            while (Prompt(out line))
+            {
+                try
+                {
+                    var parsed = ExpressionParser.ParseExpression(line);
+                    Console.WriteLine("Parsed as {0}", parsed);
+                    Console.WriteLine("Value is {0}", parsed.Compile()());
+                }
+                catch (ParseException ex)
+                {
+                    Console.WriteLine("There was a problem with your input: {0}", ex.Message);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static bool Prompt(out string value)
+        {
+            Console.Write("Enter a numeric expression, or 'q' to quit: ");
+            var line = Console.ReadLine() ?? "";
+
+            if (line.ToLowerInvariant().Trim() == "q")
+            {
+                value = line;
+                return false;
+            }
+            else
+            {
+                value = line;
+                return true;
+            }
+        }
+    }
+}
